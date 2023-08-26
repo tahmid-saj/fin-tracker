d46bdf2a70df9cafb1d54407a2a6b636892b49bd
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-26 15:16:17 -0400

    Authentication routes

--- fin-tracker/src/App.js
+++ fin-tracker/src/App.js
@@ -2,17 +2,21 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import Banking from "./routes/banking/banking.component";
+import Investment from "./routes/investment/investment.component";
+import Savings from "./routes/savings/savings.components";
+import Authentication from "./routes/authentication/authentication.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={ <Navigation/> }>
         <Route index element={ <Home/> }/>
-        {/* <Route path="banking" element={ <Banking/> }/>
+        <Route path="banking" element={ <Banking/> }/>
         <Route path="investment" element={ <Investment/> }/>
         <Route path="savings" element={ <Savings/> }/>
 
-        <Route path="auth" element={ <Authentication/> }/> */}
+        <Route path="auth" element={ <Authentication/> }/>
       </Route>
     </Routes>
   );
---
+++ fin-tracker/src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,11 @@
+import "./authentication.styles.scss";
+
+const Authentication = () => {
+  return (
+    <div className="authentication-container">
+      <h1>Sign in | Sign up</h1>
+    </div>
+  );
+};
+
+export default Authentication;
