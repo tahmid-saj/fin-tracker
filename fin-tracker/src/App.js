e4cb63e34cf817c2a301e5b5993e7be07d9786b9
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-26 14:23:42 -0400

    Adding navigation

--- fin-tracker/src/App.js
+++ fin-tracker/src/App.js
@@ -1,5 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
 const App = () => {
   return (
     <Routes>
