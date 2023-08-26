95859f68667f1937ef498b00fec0c7ae24f84217
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-26 17:40:14 -0400

    sign-in component

--- fin-tracker/src/components/sign-in-form/sign-in-form.component.jsx
+++ fin-tracker/src/components/sign-in-form/sign-in-form.component.jsx
@@ -3,11 +3,74 @@ import { useState } from "react";
 import { signInWithGooglePopup,
         signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
 
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
 import "./sign-in-form.styles.scss";
 
+const defaultFormFields = {
+  email: "",
+  password: ""
+};
+
 const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    await signInWithGooglePopup();
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { user } = await signInAuthUserWithEmailAndPassword(email, password);
+
+      resetFormFields();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("incorrect password for email");
+          break;
+        case "auth/user-not-found":
+          alert("no user associated with this email");
+          break;
+        default:
+          console.log(error);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
   return (
-    <h1>Sign in</h1>
+    <div className="sign-in-container">
+      <h2>Sign In</h2>
+      
+      <form onSubmit={ handleSubmit }>
+        <FormInput label="Email" type="email" required onChange={ handleChange }
+                  name="email" value={ email }/>
+
+        <FormInput label="Password" type="password" required onChange={ handleChange }
+                  name="password" value={ password }/>
+
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button buttonType="google" type="button"
+                  onClick={ signInWithGoogle }>Google Sign In</Button>
+        </div>                  
+      </form>
+
+    </div>
   );
 };
 
