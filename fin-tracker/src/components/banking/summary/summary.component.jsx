815f07c5b7ab59944e90a6b840ceda1881f090ce
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-27 03:43:30 -0400

    Adjusting fonts

--- fin-tracker/src/components/banking/summary/summary.component.jsx
+++ fin-tracker/src/components/banking/summary/summary.component.jsx
@@ -4,7 +4,7 @@ const date = new Date();
 let currentDay= String(date.getDate()).padStart(2, '0');
 let currentMonth = String(date.getMonth()+1).padStart(2,"0");
 let currentYear = date.getFullYear();
-let currentDate = `${currentYear}-${currentDay}-${currentMonth}`;
+let currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 
 const Summary = () => {
   return (
