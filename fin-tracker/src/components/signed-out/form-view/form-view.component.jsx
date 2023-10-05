4240b7662f65326069576f7e9fff504a5a794e26
tahmid-saj <tahmid.saj@gmail.com>
2023-10-05 01:56:53 -0400

    multiple savings accounts

--- fin-tracker/src/components/signed-out/form-view/form-view.component.jsx
+++ fin-tracker/src/components/signed-out/form-view/form-view.component.jsx
@@ -4,6 +4,8 @@ import BankAccountForm from "../banking/bank-account-form/bank-account-form.comp
 
 import InvestmentForm from "../investments/investment-form/investment-form.component";
 
+import SavingsAccountForm from "../savings/savings-account-form/savings-account-form.component";
+
 import "./form-view.styles.scss";
 
 const FormView = ({ financeItemLabel, financeItemName }) => {
@@ -11,6 +13,7 @@ const FormView = ({ financeItemLabel, financeItemName }) => {
     <div className="form-view-container">
       { financeItemLabel === "Bank Accounts" && <BankAccountForm financeItemName={ financeItemName }></BankAccountForm> }
       { financeItemLabel === "Investments" && <InvestmentForm></InvestmentForm> }
+      { financeItemLabel === "Savings Accounts" && <SavingsAccountForm></SavingsAccountForm> }
     </div>
   );
 }
