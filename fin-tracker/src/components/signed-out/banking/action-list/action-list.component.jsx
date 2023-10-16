2920bf9b975d0c16d338a375a1f2aff4f62f9def
tahmid-saj <tahmid.saj@gmail.com>
2023-10-15 23:11:25 -0400

    updating bank account deposits with banking context

--- fin-tracker/src/components/signed-out/banking/action-list/action-list.component.jsx
+++ fin-tracker/src/components/signed-out/banking/action-list/action-list.component.jsx
@@ -5,7 +5,25 @@ import CloseAccount from "../close-account/close-account.component";
 
 import "./action-list.styles.scss";
 
-const ActionList = ({ closeAccountHandler, newTransactionHandler }) => {
+const ActionList = ({ closeAccountHandler, financeItemInfo
+  // newTransactionHandler 
+}) => {
+  return (
+    <div className="action-list-container">
+      <Deposit 
+        // newTransactionHandler={ newTransactionHandler }
+        financeItemInfo={ financeItemInfo }
+        ></Deposit>
+      <Withdraw 
+        // newTransactionHandler={ newTransactionHandler }
+      ></Withdraw>
+      <TransferMoney></TransferMoney>
+      <CloseAccount closeAccountHandler={ closeAccountHandler }></CloseAccount>
+    </div>
+  );
+};
+
+const ActionList2 = ({ closeAccountHandler, newTransactionHandler }) => {
   return (
     <div className="action-list-container">
       <Deposit newTransactionHandler={ newTransactionHandler }></Deposit>
