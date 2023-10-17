631aa1f9dfd316c5b5907b5f03335b8931b81532
tahmid-saj <tahmid.saj@gmail.com>
2023-10-16 23:09:05 -0400

    summary for investments

--- fin-tracker/src/components/signed-out/investments/investments.component.jsx
+++ fin-tracker/src/components/signed-out/investments/investments.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import FinanceTrackerItems from "../../shared/finance-tracker-items/finance-tracker-items.component";
 
 import "./investments.styles.scss";
@@ -6,6 +6,9 @@ import UpdateInvestmentForm from "./update-investment-form/update-investment-for
 import InvestmentInfo from "./investment-info/investment-info.component";
 import Summary from "./summary/summary.component";
 import CreateInvestmentForm from "./create-investment-form/create-investment-form.component";
+import AllInvestmentsSummary from "./all-investments-summary/all-investments-summary.component";
+
+import { InvestmentsContext } from "../../../contexts/signed-out/investments/investments.context";
 
 const FINANCE_ITEM_TYPE = "Investments";
 
@@ -13,10 +16,16 @@ const financeTrackerItemNames = [
 ]
 
 const Investments = () => {
+  const { investments } = useContext(InvestmentsContext);
+
   // render() {
     return (
       <div className="investments-container">
         {/* <FinanceTrackerItems label={ FINANCE_ITEM_TYPE } financeTrackerItemNames={ financeTrackerItemNames }></FinanceTrackerItems> */}
+
+        {
+          investments.length !== 0 && <AllInvestmentsSummary></AllInvestmentsSummary>
+        }
 
         <div className="investments-form-summary-container">
           <CreateInvestmentForm label={ FINANCE_ITEM_TYPE } 
