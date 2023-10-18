a2be2b5b6b13582e74eef1e44ddf7a38ec0c0d48
tahmid-saj <tahmid.saj@gmail.com>
2023-10-17 21:32:54 -0400

    summaries for dashboard

--- fin-tracker/src/components/signed-in/summary/investments/investments-summary.component.jsx
+++ fin-tracker/src/components/signed-in/summary/investments/investments-summary.component.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./investments-summary.styles.scss";
 // import FinanceTrackerItemSummary from "../../../shared/finance-tracker-item-summary/finance-tracker-item-summary.component";
 import FinanceTrackerItemSummary from "../../finance-tracker-item-summary/finance-tracker-item-summary.component";
 
+import { InvestmentsContext } from "../../../../contexts/signed-in/investments/investments.context";
+
+const FINANCE_ITEM_TYPE = "Investments";
+
 const InvestmentsSummary = () => {
+  const { investments } = useContext(InvestmentsContext);
+
   return (
     <div className="investments-summary-container">
       <h2 style={{color: "white"}}><strong>Investments Summary</strong></h2>
-      <FinanceTrackerItemSummary financeTrackerItemName={ "GIC" }></FinanceTrackerItemSummary>
+
+      {
+        investments.map(investment => {
+          return <FinanceTrackerItemSummary financeTrackerItemInfo={ investment }
+                                            financeItemType={ FINANCE_ITEM_TYPE }></FinanceTrackerItemSummary>
+        })
+      }
     </div>
   );
 };
--- fin-tracker/src/components/signed-in/summary/savings/savings-summary.component.jsx
+++ fin-tracker/src/components/signed-in/summary/savings/savings-summary.component.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./savings-summary.styles.scss";
 // import FinanceTrackerItemSummary from "../../../shared/finance-tracker-item-summary/finance-tracker-item-summary.component";
 import FinanceTrackerItemSummary from "../../finance-tracker-item-summary/finance-tracker-item-summary.component";
 
+import { SavingsContext } from "../../../../contexts/signed-in/savings/savings.context";
+
+const FINANCE_ITEM_TYPE = "Savings Accounts";
+
 const SavingsSummary = () => {
+  const { savingsAccounts } = useContext(SavingsContext);
+
   return (
     <div className="savings-summary-container">
       <h2 style={{color: "white"}}><strong>Savings Summary</strong></h2>
-      <FinanceTrackerItemSummary financeTrackerItemName={ "EQ Bank" }></FinanceTrackerItemSummary>
+
+      {
+        savingsAccounts.map(account => {
+          return <FinanceTrackerItemSummary financeTrackerItemInfo={ account }
+                                            financeItemType={ FINANCE_ITEM_TYPE }></FinanceTrackerItemSummary>
+        })
+      }
     </div>
   );
 };
