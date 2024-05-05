99a5d04c0879ce8fbdea39436c5779aea430e997
tahmid-saj <tahmid.saj@gmail.com>
2024-05-04 22:03:41 -0400

    expenses routes and nav

--- fin-tracker/src/App.js
+++ fin-tracker/src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import HomeRoute from "./routes/shared/home/home.component";
 import Navigation from "./routes/shared/navigation/navigation.component";
+import ExpensesRoute from "./routes/signed-out/expenses/expenses.component";
 import BankingRoute from "./routes/signed-out/banking/banking.component";
 import InvestmentsRoute from "./routes/signed-out/investments/investments.components";
 import SavingsRoute from "./routes/signed-out/savings/savings.components";
 import AuthenticationRoute from "./routes/signed-out/authentication/authentication.component";
 
 import DashboardRouteSignedIn from "./routes/signed-in/dashboard/dashboard.component";
+import ExpensesRouteSignedIn from "./routes/signed-in/expenses/expenses.component";
 import BankingRouteSignedIn from "./routes/signed-in/banking/banking.component";
 import InvestmentsRouteSignedIn from "./routes/signed-in/investments/investments.components";
 import SavingsRouteSignedIn from "./routes/signed-in/savings/savings.components";
@@ -18,11 +20,13 @@ const App = () => {
     <Routes>
       <Route path="/" element={ <Navigation/> }>
         <Route index element={ <HomeRoute/> }/>
+        <Route path="expenses" element={ <ExpensesRoute/> }/>
         <Route path="banking" element={ <BankingRoute/> }/>
         <Route path="investments" element={ <InvestmentsRoute/> }/>
         <Route path="savings" element={ <SavingsRoute/> }/>
         <Route path="auth" element={ <AuthenticationRoute/> }/>
 
+        <Route path="expenses-signed-in" element={ <ExpensesRouteSignedIn/> }/>
         <Route path="dashboard-signed-in" element={ <DashboardRouteSignedIn/> }/>
         <Route path="banking-signed-in" element={ <BankingRouteSignedIn/> }/>
         <Route path="investments-signed-in" element={ <InvestmentsRouteSignedIn/> }/>
---
+++ fin-tracker/src/routes/signed-in/expenses/expenses.component.jsx
@@ -0,0 +1,11 @@
+import "./expenses.styles.scss"
+
+const ExpensesRouteSignedIn = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ExpensesRouteSignedIn
