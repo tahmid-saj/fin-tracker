d1aa4238b7d94e90d555e19a10c7405671fa9102
tahmid-saj <tahmid.saj@gmail.com>
2024-05-05 20:55:48 -0400

    finishing expenses filter

--- fin-tracker/src/components/signed-out/expenses/expenses.component.jsx
+++ fin-tracker/src/components/signed-out/expenses/expenses.component.jsx
@@ -4,8 +4,12 @@ import ExpensesGraph from "./expenses-graph/expenses-graph.component"
 import ExpensesTable from "./expenses-table/expenses-table.component"
 import ExpensesSummary from "./expenses-summary/expenses-summary.component"
 import "./expenses.styles.scss"
+import { useContext, Fragment } from "react"
+import { ExpensesContext } from "../../../contexts/signed-out/expenses/expenses.context"
 
 const Expenses = () => {
+  const { expenses, expensesView } = useContext(ExpensesContext)
+
   return (
     <div className="expenses-container">
       <div className="expenses-add-filter-container">
@@ -13,12 +17,22 @@ const Expenses = () => {
         <ExpensesFilter></ExpensesFilter>
       </div>
 
-      <div className="expenses-info">
-        <h1>Summary</h1>
-        <ExpensesSummary></ExpensesSummary>
-        <ExpensesGraph></ExpensesGraph>
-        <ExpensesTable></ExpensesTable>
-      </div>
+      {
+        expenses.length ?
+        <div className="expenses-info">
+          <h1>Summary</h1>
+          <ExpensesSummary></ExpensesSummary>
+
+          {
+            expensesView.length ?
+              <Fragment>
+                <ExpensesGraph></ExpensesGraph>
+                <ExpensesTable></ExpensesTable>
+              </Fragment> : null
+          }
+        </div> : null
+      }
+
     </div>
   )
 }
