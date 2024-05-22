d407aa9ae822f6030d90a1eb1696a4690b435fe8
tahmid-saj <tahmid.saj@gmail.com>
2024-05-21 20:36:43 -0400

    initializing redux store

---
+++ src/store/middleware/logger.js
@@ -0,0 +1,15 @@
+// redux middlewares
+
+export const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action)
+  }
+
+  console.log("type ", action.type)
+  console.log("payload ", action.payload)
+  console.log("currentState ", store.getState())
+
+  next(action)
+
+  console.log("next state ", store.getState())
+}
