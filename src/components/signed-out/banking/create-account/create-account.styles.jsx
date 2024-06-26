adea0771468f6a39a45c68648e9b4f397d8fe750
tahmid-saj <tahmid.saj@gmail.com>
2024-06-26 14:43:13 -0400

    banking styling

---
+++ src/components/signed-out/banking/create-account/create-account.styles.jsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const CreateAccountContainer = styled.div`
+  margin: 0% 2% 2% 2%;
+`
--- src/pages/signed-out/banking/banking.component.jsx
+++ src/pages/signed-out/banking/banking.component.jsx
@@ -6,13 +6,20 @@ import { setBankingSummary } from "../../../store/signed-out/banking/banking.act
 
 import BankAccounts from "../../../components/signed-out/banking/bank-accounts/bank-accounts.component";
 
-import "./banking.styles.scss";
+import "./banking.styles.jsx";
+import { BankingContainer } from "./banking.styles.jsx";
 
 // import { BankingContext } from "../../../contexts/signed-out/banking/banking.context";
 import AllBankingSummary from "../../../components/signed-out/banking/all-banking-summary/all-banking-summary.component";
 
 import { FINANCE_ITEM_TYPES } from "../../../utils/constants/shared.constants";
 import SummaryGraphBanking from "../../../components/signed-out/banking/summary-graph/summary-graph.component";
+
+import SummarizeIcon from '@mui/icons-material/Summarize';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance'
+import AddIcon from '@mui/icons-material/Add';
+import CreateAccount from "../../../components/signed-out/banking/create-account/create-account.component.jsx";
+import ItemTabs from "../../../components/shared/mui/tabs/tabs.component.jsx";
 
 const Banking = () => {
   // const { bankingAccounts } = useContext(BankingContext);
@@ -31,18 +38,59 @@ const Banking = () => {
     }))
   }, [bankingAccounts, dispatch])
 
-  return (
-    <div className="banking-container">
-      {
-        bankingAccounts.length !== 0 && 
-        <Fragment>
-          <AllBankingSummary></AllBankingSummary>
-        </Fragment>
-      }
+  let tabList = []
+  let panelList = []
 
-      <BankAccounts label={ FINANCE_ITEM_TYPES.banking }></BankAccounts>
-    </div>
-  );
+  if (bankingAccounts && bankingAccounts.length !== 0) {
+    tabList.push({
+      value: "summary",
+      icon: <SummarizeIcon/>,
+      label: "Summary"
+    })
+    tabList.push({
+      value: "bank-accounts",
+      icon: <AccountBalanceIcon/>,
+      label: "Bank Accounts"
+    })
+
+    panelList.push({
+      value: "summary",
+      children: <AllBankingSummary/>
+    })
+    panelList.push({
+      value: "bank-accounts",
+      children: <BankAccounts/>
+    })
+  }
+
+  tabList.push({
+    value: "add-account",
+    icon: <AddIcon/>,
+    label: "Add Account"
+  })
+  panelList.push({
+    value: "add-account",
+    children: <CreateAccount/>
+  })
+
+  return (
+    <BankingContainer>
+      <ItemTabs tabList={ tabList } panelList={ panelList }></ItemTabs>
+    </BankingContainer>
+  )
+
+  // return (
+  //   <div className="banking-container">
+  //     {
+  //       bankingAccounts.length !== 0 && 
+  //       <Fragment>
+  //         <AllBankingSummary></AllBankingSummary>
+  //       </Fragment>
+  //     }
+
+  //     <BankAccounts label={ FINANCE_ITEM_TYPES.banking }></BankAccounts>
+  //   </div>
+  // );
 };
 
 export default Banking;
