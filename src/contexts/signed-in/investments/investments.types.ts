6f83a24f6e26ffe7d8948b2ac53f4b8921f2cbf8
tahmid-saj <tahmid.saj@gmail.com>
2024-09-21 01:48:15 -0400

    migrating insurance context to ts

---
+++ src/contexts/signed-in/investments/investments.types.ts
@@ -0,0 +1,50 @@
+import { ReactNode } from "react";
+
+// investment types
+
+export type Investment = {
+  investmentName: string;
+  investmentType: string;
+  startingAmount: number;
+  startDate: string;
+  afterYears: number;
+  returnRate: number;
+  compounded: string;
+  additionalContribution: number;
+  contributionAt: string;
+  contributionInterval: string;
+
+  endBalance: number;
+  totalContribution: number;
+  totalInterest: number;
+
+  investments: InvestmentCalculationRecord[];
+}
+
+export interface InvestmentContextType {
+  investments: Investment[];
+
+  createInvestment: (investment: Investment) => void;
+  updateInvestment: (originalInvestmentName: string, updatedInvestment: Investment) => void;
+  closeInvestment: (closingInvestmentName: string) => void;
+  getInvestmentInfo: (investmentName: string) => void;
+
+  investmentsSummary: InvestmentsSummary;
+}
+
+export interface InvestmentProviderProps {
+  children: ReactNode;
+}
+
+export type InvestmentCalculationRecord = {
+  currentDate: string;
+  contribution: number;
+  interestAccumulated: number;
+  endingBalance: number;
+}
+
+export type InvestmentsSummary = {
+  currentAllInvestmentsBalance?: number;
+  totalAllContribution?: number;
+  totalAllInterest?: number;
+}
