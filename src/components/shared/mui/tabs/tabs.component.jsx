c029c67e56e024357543c5f7c38c63932699d931
tahmid-saj <tahmid.saj@gmail.com>
2024-09-21 15:33:41 -0400

    migrating routes to ts

--- src/components/shared/mui/tabs/tabs.component.jsx
+++ src/components/shared/mui/tabs/tabs.component.jsx
@@ -8,7 +8,7 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 
-export default function ItemTabs({ outerBoxStyles, innerBoxStyles, tabList, panelList }) {
+export default function ItemTabs({ outerBoxStyles={}, innerBoxStyles={}, tabList, panelList }) {
   const [value, setValue] = useState(tabList[0].value);
 
   const handleChange = (event, newValue) => {
